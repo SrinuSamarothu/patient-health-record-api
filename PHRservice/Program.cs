5ce27219f0c1cca7a5b9c743d920fb201f4e7013
manojkmj11 <mithuna692@gmail.com>
2023-04-04 22:00:21 +0530

    updated

--- PHRservice/Program.cs
+++ PHRservice/Program.cs
@@ -29,11 +29,11 @@ builder.Services.AddScoped<ILogic, Logic>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
